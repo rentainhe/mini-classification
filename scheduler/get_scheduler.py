648a160b8fa03dd6f51fc5be670e3991ef408cee
rentainhe <596106517@qq.com>
2021-01-19 18:27:42 +0800

    debug

--- scheduler/get_scheduler.py
+++ scheduler/get_scheduler.py
@@ -3,6 +3,6 @@ from scheduler.scheduler import WarmupCosineSchedule
 
 def get_scheduler(__C, optimizer):
     if __C.training['lr_scheduler'] == 'Cosine':
-        return WarmupCosineSchedule(optimizer=optimizer, warmup_steps=__C.training['warmup_steps'], t_total=__C.training['total_steps'])
+        return WarmupCosineSchedule(optimizer=optimizer, warmup_steps=__C.training['warmup_steps'], t_total=__C.training['max_steps'])
     elif __C.training['lr_scheduler'] == 'Linear':
-        return WarmupLinearSchedule(optimizer=optimizer, warmup_steps=__C.training['warmup_steps'], t_total=__C.training['total_steps'])
+        return WarmupLinearSchedule(optimizer=optimizer, warmup_steps=__C.training['warmup_steps'], t_total=__C.training['max_steps'])
