e1c1c52044f5530a95a362d8d2f9abddc42c0885
rentianhe <596106517@qq.com>
2021-02-06 17:24:28 +0800

    add auto batch_size finder

--- example/example.py
+++ example/example.py
@@ -16,13 +16,14 @@ from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.callbacks import ModelCheckpoint
 from PIL import Image
 from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning import metrics
 
 # transforms
 # prepare transforms standard to MNIST
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.1307,),(0.3081,))])
 mnist_train = MNIST(os.getcwd(), train=True, download=True, transform=transform)
-mnist_train = DataLoader(mnist_train, batch_size=64)
+# mnist_train = DataLoader(mnist_train, batch_size=64)
 
 mnist_val = MNIST(os.getcwd(), train=False, download=True, transform=transform)
 mnist_val = DataLoader(mnist_val, batch_size=64)
@@ -60,33 +61,53 @@ class StandardMNIST(nn.Module):
 
 # extend StandardMNIST and LightningModule at the same time
 # this is what I like from python, extend two class at the same time
-class ExtendMNIST(StandardMNIST,LightningModule):
+class ExtendMNIST(StandardMNIST, LightningModule):
     def __init__(self, __C=None):
         super().__init__()
-
-
+        self.train_acc = metrics.Accuracy()
+        self.valid_acc = metrics.Accuracy()
+        self.batch_size = 64
+        self.mnist_train = mnist_train
     def training_step(self, batch, batch_idx):
         data, target = batch
         logits = self.forward(data)
         loss = F.nll_loss(logits, target)
+        self.train_acc(logits, target)
+        self.log('train_acc', self.train_acc, on_step=False, on_epoch=True)
         return {'loss': loss}
 
-    # def validation_step(self, batch, batch_idx):
-    #     print("validation")
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch
+        test_outputs = self(images)
+        loss = F.cross_entropy(test_outputs, labels)
+        _, pred = test_outputs.topk(5, 1, largest=True, sorted=True)
+        labels = labels.view(labels.size(0), -1).expand_as(pred)
+        correct = pred.eq(labels).float()
+
+        # top-5
+        correct_5 = correct[:, :5].sum()
+
+        # top-1
+        correct_1 = correct[:, :1].sum()
+        self.log_dict({'test_loss': loss, 'top-1': correct_1, 'top-5': correct_5}, on_epoch=True)
 
     def configure_optimizers(self):
         optimier = torch.optim.SGD(self.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
         train_sched = {
             'scheduler': WarmupCosineSchedule(optimier, warmup_steps=1000, t_total=80000),
             'interval': 'step',
-            'frequency': 2
+            'frequency': 1
         }
         return {'optimizer': optimier, 'lr_scheduler': train_sched}
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size)
 
 model = ExtendMNIST()
 lr_monitor = LearningRateMonitor(logging_interval='step')
 
-tb_logger = pl_loggers.TensorBoardLogger(save_dir='logs/', name='test',version='test')
+tb_logger = pl_loggers.TensorBoardLogger(save_dir='logs/', name='metrics_test',version='metrics2')
 
-trainer = Trainer(max_steps=90000, callbacks=[lr_monitor], logger=tb_logger, fast_dev_run=True)
+trainer = Trainer(max_steps=90000, callbacks=[lr_monitor], logger=tb_logger, auto_scale_batch_size='power')
+trainer.tune(model)
 trainer.fit(model, mnist_train, mnist_val)
